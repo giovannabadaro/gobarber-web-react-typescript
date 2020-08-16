48813cf850bb10058e558292752bf1cb04edba3f
Giovanna <giovannabadaroalves@gmail.com>
2020-08-16 16:36:31 -0300

    input + button component

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+
 const Signin: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="go barbber"/>
+        <img src={logoImg} alt=""/>
 
         <form action="">
           <h1>Faça seu logon</h1>
 
-          <input placeholder="E-mail" />
+          <Input name= "email" icon={FiMail} placeholder="E-mail" />
 
-          <input type="password" placeholder="Senha" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
         </form>
@@ -27,6 +31,7 @@ const Signin: React.FC = () => {
           <FiLogIn />
           Criar conta
         </a>
+        
       </Content>
 
       <Background />       
