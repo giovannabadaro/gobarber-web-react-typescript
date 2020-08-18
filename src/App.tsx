7c78867ff2a7e8d6b58cfd30e43bfc1858c20047
Giovanna <giovannabadaroalves@gmail.com>
2020-08-18 11:42:22 -0300

    API context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+
 import GlobalStyle from './styles/global';
 import Signin from '../src/pages/Signin';
 import Signup from '../src/pages/Signup';
 
+import {AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <Signin />
+    <AuthProvider>
+      <Signin />
+    </AuthProvider>
+    
 
   </>  
 );
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,11 +14,19 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 
 const Signin: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async(data:object) => {
+  const { signIn } = useContext(AuthContext);
+
+   
+  const handleSubmit = useCallback(async(data:SignInFormData) => {
     try{
       formRef.current?.setErrors({});
 
@@ -28,14 +37,19 @@ const Signin: React.FC = () => {
 
       await schema.validate(data, {
         abortEarly: false,
-      });      
+      });  
+      signIn({
+        email: data.email,
+        password: data.password,
+      });    
     }catch (err){
 
       const errors = getValidationErrors(err);
 
       formRef.current?.setErrors(errors);
     }
-  },[]);
+    
+  },[signIn]);
 
   return (
     <Container>
